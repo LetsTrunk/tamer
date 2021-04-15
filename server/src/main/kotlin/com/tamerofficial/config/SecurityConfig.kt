095a8f0762973a48dd6d670289e1d74998c4ec04
jun <tpwns107@gmail.com>
2021-04-15 23:30:41 +0900

    Add OAuth2 configure on SpringSecurity

--- server/src/main/kotlin/com/tamerofficial/config/SecurityConfig.kt
+++ server/src/main/kotlin/com/tamerofficial/config/SecurityConfig.kt
@@ -3,6 +3,7 @@ package com.tamerofficial.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
@@ -13,17 +14,21 @@ import org.springframework.security.web.server.SecurityWebFilterChain
 class SecurityConfig {
     @Bean
     fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain? {
-        http.csrf()
-            .disable()
+        http
+            .csrf().disable()
+            .httpBasic().disable()
+            .formLogin().disable()
             .authorizeExchange()
-            .pathMatchers(HttpMethod.POST, "/employees/update")
-            .hasRole("ADMIN")
-            .pathMatchers("/**") // 임시로 다 풀어둠
-            .permitAll()
-            .pathMatchers("/actuator/**")
-            .permitAll()
+//            .pathMatchers("/**") // 임시로 다 풀어둠
+//            .permitAll()
+            .pathMatchers("/api/**").authenticated()    // /api 하위 경로는 인증 필요, 인증안되었다면 로그인 이동
+            .pathMatchers(HttpMethod.POST, "/employees/update").hasRole("ADMIN")
+            .pathMatchers("/actuator/**").permitAll()
+            .anyExchange().permitAll()
             .and()
-            .formLogin()
+            .oauth2Login(Customizer.withDefaults())
+            .oauth2Client(Customizer.withDefaults())
+            .logout()
 
         return http.build()
     }
