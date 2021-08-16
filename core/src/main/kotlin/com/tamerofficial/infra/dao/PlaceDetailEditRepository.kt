39c9bd8476a279e820ac2ee5c05781d48665204b
Jinhan Choi <jinhanchoi@Jinhanui-MacBookPro.local>
2021-08-16 22:23:17 +0900

    remove unuse repo and entity

--- core/src/main/kotlin/com/tamerofficial/infra/dao/PlaceDetailEditRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.tamerofficial.infra.dao
-
-import com.tamerofficial.infra.entity.PlacesEntity
-import org.springframework.data.repository.reactive.ReactiveCrudRepository
-
-interface PlaceDetailEditRepository : ReactiveCrudRepository<PlacesEntity,Long>{
-}
